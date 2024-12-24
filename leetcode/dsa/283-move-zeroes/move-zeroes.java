1686470aa47a763e06a8a6c141fb7a1b072cbdd6
Bobandeep Singh <bobandeepsingh@gmail.com>
2024-12-25 02:42:12 +0530

    Time: 2 ms (84.98%) | Memory: 45.9 MB (48.64%) - LeetSync

--- leetcode/dsa/283-move-zeroes/move-zeroes.java
+++ leetcode/dsa/283-move-zeroes/move-zeroes.java
@@ -3,17 +3,16 @@ class Solution {
         if(nums.length <= 1){
             return;
         }
-        int l = 0;
-        int r = 0;
-        while(r < nums.length){
-            // System.out.println(l + " & " + r + " - " + nums[l] + " & " + nums[r]);
-            if(nums[r] != 0){
-                int temp = nums[l];
-                nums[l] = nums[r];
-                nums[r] = temp;
-                l++;
+        int zeroIndex = 0;
+        int currentIndex = 0;
+        while(currentIndex < nums.length){
+            if(nums[currentIndex] != 0){
+                int temp = nums[zeroIndex];
+                nums[zeroIndex] = nums[currentIndex];
+                nums[currentIndex] = temp;
+                zeroIndex++;
             } 
-            r++;
+            currentIndex++;
         }
 
     }
