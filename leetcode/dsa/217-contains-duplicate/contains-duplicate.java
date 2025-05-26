ae711a58731ff903b0a1690291200bdd225e9579
Bobandeep Singh <bobandeepsingh@gmail.com>
2025-05-26 05:33:16 +0530

    Time: 13 ms (71.47%) | Memory: 58.5 MB (43.15%) - LeetSync

--- leetcode/dsa/217-contains-duplicate/contains-duplicate.java
+++ leetcode/dsa/217-contains-duplicate/contains-duplicate.java
@@ -1,6 +1,17 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
+        HashSet<Integer> aSet = new HashSet<>();
+        for(int i: nums){
+            if(aSet.contains(i)) return true;
+            else aSet.add(i);
+        }
+        return false;
+    }
+
+/*
+    public boolean containsDuplicate(int[] nums) {
+
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i: nums){
             if(null != map.get(i)) return true;
@@ -8,4 +19,5 @@ class Solution {
         }
         return false;
     }
+*/
 }
